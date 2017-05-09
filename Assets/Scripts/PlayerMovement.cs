e423ae438cc549f901acacd0e4c6bf999a4442ac
Aram Joseph <ajoseph@utc4me.org>
2017-05-08 22:43:15 -0400

    woot

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,30 +5,24 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 6f;
 
     Vector3 movement;
-    //Animator anim;
     Rigidbody playerRigidbody;
     int floorMask;
     float camRayLength = 100f;
-    private TestManager projectileManager;
 
     void Awake()
     {
-        projectileManager = GetComponent<TestManager>();
+
         floorMask = LayerMask.GetMask("Floor");
-        //anim = GetComponent<Animator>();
         playerRigidbody = GetComponent<Rigidbody>();
     }
 
     void FixedUpdate()
     {
-        
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
 
         Move(h, v);
         Turning();
-        Attack();
-        //Animating(h, v);
     }
 
     void Move(float h, float v)
@@ -55,18 +49,4 @@ public class PlayerMovement : MonoBehaviour
             playerRigidbody.MoveRotation(newRotation);
         }
     }
-
-    void Attack()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            projectileManager.Shoot();
-        }
-    }
-
-    //void Animating(float h, float v)
-    //{
-    //    bool walking = h != 0f || v != 0f;
-    //    anim.SetBool("IsWalking", walking);
-    //}
 }
